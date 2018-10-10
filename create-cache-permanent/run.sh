80d7d800eee89df6e78da3fe7c06c9b870e82826
Galder Zamarreño <galder@zamarreno.com>
2018-10-10 16:31:04 +0200

    Get image name dynamically

--- create-cache-permanent/run.sh
+++ create-cache-permanent/run.sh
@@ -3,21 +3,24 @@
 #set -e -x
 set -e
 
-#oc delete all,secrets,sa,templates,configmaps,daemonsets,clusterroles,rolebindings,serviceaccounts--selector=run=app || true
 oc delete all --selector=run=app || true
+
+getImageName() {
+  oc get is app  -o jsonpath="{.status.dockerImageRepository}"
+}
+
 oc run app \
-  --image=172.30.1.1:5000/myproject/app \
+  --image=$(getImageName) \
   --replicas=1 \
   --restart=OnFailure \
   --env PARAM=RedHat
+
 
 getPodStatus() {
   oc get pod -l run=app -o jsonpath="{.items[0].status.phase}"
 }
 
-#expected=Succeeded
 status=NA
-
 while [ "$status" != "Succeeded" ];
 do
   status=$(getPodStatus)
@@ -25,11 +28,9 @@ do
   sleep .5
 done
 
-#while [[ "${status}" -ne "${expected}" ]]; do
-#  sleep .5
-#  status=$(getPodStatus)
-#  echo "status of pod: ${status}"
-#done
 
-POD=$(oc get pod -l run=app -o jsonpath="{.items[0].metadata.name}")
-oc logs $POD
+getPodName() {
+  oc get pod -l run=app -o jsonpath="{.items[0].metadata.name}"
+}
+
+oc logs $(getPodName)
